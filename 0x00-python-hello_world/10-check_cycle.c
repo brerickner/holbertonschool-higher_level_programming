25f4096c37f29e8c4c8a1689c8a22c62e2ef8323
brerickner <breannarickner@gmail.com>
2020-12-07 15:33:12 +0000

    saving

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,1 +1,11 @@
 #include "lists.h"
+/**
+ * int check_cycle - listint_t *list
+ * @list: linked list of ints
+ * description: check to see if singly linked list has a cycle
+ * Return: 0 if no cycle. Else 1.
+ */
+int check_cycle(listint_t *list)
+{
+	return (0);
+}
