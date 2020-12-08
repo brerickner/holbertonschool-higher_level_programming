625cd90bb086c554b2fac5d0f8882f2a932e72b3
brerickner <breannarickner@gmail.com>
2020-12-08 19:32:15 +0000

    oops indents and /

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+obfuscate#!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
 if number < 10:
@@ -6,10 +6,10 @@ if number < 10:
 else:
     last_dig = number % 10
 if last_dig > 5:
-    print("last digit of {} is {} and is greater than 5"\
+    print("last digit of {} is {} and is greater than 5"
           .format(number, last_dig))
 elif last_dig == 0:
     print("last digit of {} is {} and is 0".format(number, last_dig))
 else:
-    print("last digit of {} is {} and is less than 6 and not 0"\
+    print("last digit of {} is {} and is less than 6 and not 0"
           .format(number, last_dig))
