0226dc5bd8d5ca3217ac853738360ef58cffa290
brerickner <breannarickner@gmail.com>
2020-12-09 04:38:57 +0000

    dummy code

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * listint_t *insert_node- listint_t **head, int number
+ * @head: pointer to head of list
+ * @number: number in nodes of list
+ * Return: address of new node. Else NULL
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	(void)number;
+
+	return (*head);
+}
