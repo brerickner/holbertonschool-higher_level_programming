3e32530384327db5c4e373023cc3587123b6b552
brerickner <breannarickner@gmail.com>
2020-12-09 02:14:32 +0000

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for numbers in range (0, 100):
-    if(numbers < 10):
-        print("0 + number{}".format(numbers), end=", ")
-    else:
-        print("{:}".format(numbers), end=", ")
+for left_most in range(0, 10):
+    for right_most in range(left_most + 1, 10):
+        if left_most == 8 and right_most == 9:
+            print("{}{}".format(left_most, right_most))
+        else:
+            print("{}{}".format(left_most, right_most), end=", ")
