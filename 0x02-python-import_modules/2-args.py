a5cd1bf9a11dd2ad088b16ec8f46136d7a22c125
brerickner <breannarickner@gmail.com>
2020-12-10 21:37:00 +0000

    name main

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
+if __name__ = "__main__":
+    from sys import argv
 
-from sys import argv
+    arguments = len(argv)
 
-arguments = len(argv)
+    # __ arguments: if args passed in
 
-# __ arguments: if args passed in
+    if arguments > 2:
+        print("{:d} arguments:".format(arguments - 1))
 
-if arguments > 2:
-    print("{:d} arguments:".format(arguments - 1))
+        # 1 argument: if 2 args passed.
 
-# 1 argument: if 2 args passed.
+    elif arguments == 2:
+        print("{:d} argument:".format(arguments - 1))
 
-elif arguments == 2:
-    print("{:d} argument:".format(arguments - 1))
+        # 0 arguments. if no args passed in
+    else:
+        print("{:d} arguments.".format(arguments - 1))
 
-# 0 arguments. if no args passed in
-else:
-    print("{:d} arguments.".format(arguments - 1))
+        # for each arg passed in, increment the position and argument in list
 
-# for each arg passed in, increment the position and argument in list
-
-for pos, arg in enumerate(argv[1:]):
-    print("{:d}: {}".format(pos + 1, arg))
+        for pos, arg in enumerate(argv[1:]):
+            print("{:d}: {}".format(pos + 1, arg))
