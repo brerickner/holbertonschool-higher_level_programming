e247a9b3b5eae9603e7e3bc7d8ffcb011278b34d
brerickner <breannarickner@gmail.com>
2020-12-10 21:35:20 +0000

    pepfix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,18 +7,18 @@ arguments = len(argv)
 # __ arguments: if args passed in
 
 if arguments > 2:
-    print("{:d} arguments:".format(arguments -1))
+    print("{:d} arguments:".format(arguments - 1))
 
-#1 argument: if 2 args passed.
+# 1 argument: if 2 args passed.
 
 elif arguments == 2:
     print("{:d} argument:".format(arguments - 1))
 
-#0 arguments. if no args passed in
+# 0 arguments. if no args passed in
 else:
     print("{:d} arguments.".format(arguments - 1))
 
-#for each arg passed in, increment the position and argument in list
+# for each arg passed in, increment the position and argument in list
 
 for pos, arg in enumerate(argv[1:]):
     print("{:d}: {}".format(pos + 1, arg))
