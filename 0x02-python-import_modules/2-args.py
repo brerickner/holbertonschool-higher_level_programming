b48711f120344a8d1905390e23478bd4d48090b3
brerickner <breannarickner@gmail.com>
2020-12-10 21:34:49 +0000

    this one was fun

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+arguments = len(argv)
+
+# __ arguments: if args passed in
+
+if arguments > 2:
+    print("{:d} arguments:".format(arguments -1))
+
+#1 argument: if 2 args passed.
+
+elif arguments == 2:
+    print("{:d} argument:".format(arguments - 1))
+
+#0 arguments. if no args passed in
+else:
+    print("{:d} arguments.".format(arguments - 1))
+
+#for each arg passed in, increment the position and argument in list
+
+for pos, arg in enumerate(argv[1:]):
+    print("{:d}: {}".format(pos + 1, arg))
