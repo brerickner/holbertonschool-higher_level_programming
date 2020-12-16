7740dbaf426162ca332930140d419556acc76861
brerickner <breannarickner@gmail.com>
2020-12-16 04:53:24 +0000

    good output

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end="")
-            if j != len(matrix[i]) - 1:
-                print(" ", end="")
-        print("")
+def divisible_by_2(my_list=[]):
+    copy = []
+    for i in my_list:
+        if my_list[i] % 2 == 0:
+            copy.append(True)
+        else:
+            copy.append(False)
+    return copy
