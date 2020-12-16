77e81c236883120c7eec1fbc9ca3177d2888e3ec
brerickner <breannarickner@gmail.com>
2020-12-16 04:57:34 +0000

    yesssssss

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end="")
-            if j != len(matrix[i]) - 1:
-                print(" ", end="")
-        print("")
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
