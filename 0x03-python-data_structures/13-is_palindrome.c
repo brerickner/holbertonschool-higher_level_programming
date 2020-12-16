83b7ce180ecf68aeafaad479775a9665eaeaea9e
brerickner <breannarickner@gmail.com>
2020-12-16 05:39:36 +0000

    dummy checks

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_listint - prints all elements of a listint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
+ */
+int is_palindrome(listint_t **head)
+{
+	if(!(*head))
+	    return (1);
+	return (0);
+}
