24e26704856b93c3fd96e51980f1d94eda713ebd
brerickner <breannarickner@gmail.com>
2020-12-17 22:15:32 +0000

    output good

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,9 +2,7 @@ def search_replace(my_list, search, replace):
     if not my_list:
         return
     new = my_list.copy()
-    for item in my_list:
+    for item in range(0, len(my_list)):
         if my_list[item] == search:
             new[item] = replace
-            return new
-        else:
-            return new
+    return new
