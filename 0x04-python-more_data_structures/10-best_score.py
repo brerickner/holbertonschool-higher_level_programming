75bb8394bc66fcb30718ee63b86fc6f8ff4ee2e9
brerickner <breannarickner@gmail.com>
2020-12-18 02:46:10 +0000

    making copy?

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    dict1 = a_dictionary
     sorted_dict = {}
-    sorted_score = sorted(a_dictionary, key=a_dictionary.get)  # [1, 3, 2]
+    sorted_score = sorted(dict1, key=dict1.get)
 
     for i in sorted_score:
-        sorted_dict[i] = a_dictionary[i]
+        sorted_dict[i] = dict1[i]
 
     return sorted_dict
