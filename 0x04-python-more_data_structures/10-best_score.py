cf461c0072e816666dace04e2f3f6505adbbca94
brerickner <breannarickner@gmail.com>
2020-12-18 03:08:57 +0000

    thank you maddi!

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    dict1 = a_dictionary
-    sorted_dict = {}
-    sorted_score = sorted(dict1, key=dict1.get)
-
-    for i in sorted_score:
-        sorted_dict[i] = dict1[i]
-
-    return sorted_dict
+    if not a_dictionary:
+        return
+    topScore = max(a_dictionary, key=a_dictionary.get)
+    return topScore
