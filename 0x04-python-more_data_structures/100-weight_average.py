731e31a7207394b998e3b27dfa025e36a311e1a2
brerickner <breannarickner@gmail.com>
2020-12-18 04:32:37 +0000

    ohhh oops forgot to increment

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,6 +5,6 @@ def weight_average(my_list=[]):
     a = 0
     b = 0
     for wt in my_list:
-        a = (wt[0] * wt[1])
-        b = wt[1]
-    return (a / b)
+        a += (wt[0] * wt[1])
+        b += wt[1]
+    return (a/b)
