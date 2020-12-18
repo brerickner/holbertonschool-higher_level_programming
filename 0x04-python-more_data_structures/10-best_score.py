7a89b4f8b8d772ec5bb290a4b282f4232d847db8
brerickner <breannarickner@gmail.com>
2020-12-18 02:42:48 +0000

    AttributeError NoneType error?

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    new = list(sorted(a_dictionary))
-    return new
+    sorted_dict = {}
+    sorted_score = sorted(a_dictionary, key=a_dictionary.get)  # [1, 3, 2]
+
+    for i in sorted_score:
+        sorted_dict[i] = a_dictionary[i]
+
+    return sorted_dict
