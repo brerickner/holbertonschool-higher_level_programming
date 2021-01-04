6d7da6fff1b4350cf98b91080777d0e279983765
brerickner <breannarickner@gmail.com>
2021-01-04 22:17:28 +0000

    correct output. pepfix

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+        value = None
+        try:
+                value = a / b
+        except (AttributeError, ZeroDivisionError):
+                return None
+        finally:
+                print("Inside result: {}".format(value))
