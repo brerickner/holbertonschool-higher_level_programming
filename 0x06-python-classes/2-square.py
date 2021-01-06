fc04f1939d463f68fbef79ec608ee968f1267577
brerickner <breannarickner@gmail.com>
2021-01-06 03:08:10 +0000

    pepfix output good finally

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,11 +11,10 @@ class Square:
         Args:
             size(int): Private attribute for size of square.
         """
+
         self.__size = size
 
         if not isinstance(size, int):
-            raise TypeError
-            print("size must be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError
-            print("size must be >= 0")
+            raise ValueError("size must be >= 0")
