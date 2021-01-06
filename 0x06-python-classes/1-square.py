7762552b3502281c1fe05ae8ba1d21348c698d92
brerickner <breannarickner@gmail.com>
2021-01-06 02:05:42 +0000

    output looks good

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""A simple module that defines a class square"""
+
+
+class Square:
+
+    """Defines a square"""
+
+    def __init__(self, size):
+        """Initialize the square.
+        Args:
+            size(int): Private attribute for size of square.
+        """
+        self.__size = size
