28fd567c929a1baaa9f9ab9bfb91ecb336b93bb7
brerickner <breannarickner@gmail.com>
2021-01-07 22:15:19 +0000

    adding progress on 0-add.py output good 9 passed 0 failed

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This is our adding module"""
+def add_integer(a, b=98):
+    """Method to add two integers.
+        Args:
+            a: integer
+            b: integer
+        Return:
+            a + b
+            """
+    if isinstance(a, (int, float)) is False:
+        raise TypeError("a must be an integer")
+    
+    elif isinstance(b, (int, float)) is False:
+        raise TypeError("b must be an integer")
+
+    return (int(a) + int(b))
