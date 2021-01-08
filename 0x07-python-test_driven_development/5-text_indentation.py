d8b1970744e2994bba2e2611f80c9cd2c3d623ef
brerickner <breannarickner@gmail.com>
2021-01-08 23:16:51 +0000

    saving progress

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A module that prints 2 new lines after '.''?'':' in a text"""
 
+
 def text_indentation(text):
     """Method that places 2 new lines after specified chars
         Args:
@@ -13,16 +14,11 @@ def text_indentation(text):
 
     if isinstance(text, str) is False:
         TypeError("text must be a string")
-    
-    for i in range(len(text)):
-        if text[i] is ":":# or "." or "?":
-            if text[i + 1] is " ":
-                text = text[:i + 1] + replaceChar + text[i+2:]
-                print(text)
 
-   
-            
-            
+    # if special char, and next char is space skip two chars then start string
+    for char in range(len(text)):
+        if text[char] is ":" or text[char] is "." or text[char] is "?":
+            if text[:char + 1] is " " or "\n":
 
-            
-
+                text = text[:char + 1] + replaceChar + text[char + 2:]
+                print(text, end="")
