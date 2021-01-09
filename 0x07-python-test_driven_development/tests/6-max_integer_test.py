d4f62f10ec890eaa65e4882d31d4f88306774fb2
brerickner <breannarickner@gmail.com>
2021-01-09 02:32:15 +0000

    saving progress

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module to unittest max int"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+
+     """Class to unittest for max_integer([..])."""
+
+     def test_empties(self):
+            """Test an empty string."""
+        self.assertEqual(max_integer(""), None)
+
+    def test_err_raises(self):
+        self.assertRaises(Exception, max_integer, ["meow", "meow", 3])
+
+    def test_for_realz(self):
+        self.assertEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer([221, 8, 77]),221)
+        self.assertEqual(max_integer([-100, -99, -10]), -10)
+        self.assertEqual(max_integer([0]), 0)
+        self.assertEqual(max_integer([99, 99, 99, 98.5]), 99)
+
+
+    
