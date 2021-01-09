ba61f341e29dcf2d9b39600bbb4acb8ae5f15aff
brerickner <breannarickner@gmail.com>
2021-01-09 06:06:35 +0000

    pepfix

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -15,7 +15,6 @@ def text_indentation(text):
 
     text2 = text.strip()
 
-
     # if special idx, and next idx is space skip two chars then start string
     for idx in range(len(text2)):
         print(text2[idx], end="")
@@ -25,4 +24,3 @@ def text_indentation(text):
             if idx < len(text2) - 1 and text2[idx + 1] is " " or text2[idx + 1] is "\n":
                 idx += 1
             idx += 1
-        
