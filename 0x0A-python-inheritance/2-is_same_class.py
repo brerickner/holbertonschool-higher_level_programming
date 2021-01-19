054ac6211b3d9c73be0ce442ee3a8bc96958b432
brerickner <breannarickner@gmail.com>
2021-01-19 22:27:11 +0000

    ouput same as main file, pepfixed, passing 10 of 10 doctests

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module for class Mylist"""
+
+
+def is_same_class(obj, a_class):
+    """Method that returns True when obj is exactly instance of class
+        Args:
+            obj: object to test if instance of class
+            a_class: class to test if obj is instance of
+        Return:
+            True when obj is exactly instance of class. Else False.
+        """
+    if (isinstance(obj, a_class)) is False:
+        return False
+    return True
