dab23a3af74397f9e01174a5373a545095af8ae1
brerickner <breannarickner@gmail.com>
2021-01-20 23:26:46 +0000

    output same as main, pepfixed, docstrings good

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """Module defining class Student"""
 
+
 class Student():
+
     """Defining class Student"""
+
     def __init__(self, first_name, last_name, age):
         """Instantiation of first/last name and age"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self):
         """Method that retrieves a dict repr of Student instance"""
         return self.__dict__
