5ecd5be64678b80967ecdf908a8f42ec92cb1f18
brerickner <breannarickner@gmail.com>
2021-01-20 22:46:05 +0000

    pepfix and docstrings good

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,13 +2,16 @@
 """Module that adds and saves Python list to file"""
 
 import sys
+import os
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 args = sys.argv[1:]
-pythonList = []
-pythonList = load_from_json_file('add_item.json')
+if path.exists("add_item.json")
+    pythonList = load_from_json_file('add_item.json')
+else:
+    pythonList = []
 
 pythonList.extend(args)
 
