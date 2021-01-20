d8793b2a17aff26d574d2deb8d8be446dd683f53
brerickner <breannarickner@gmail.com>
2021-01-20 02:40:35 +0000

    output same as example, pepfixed, docstrings good

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module defining Square class"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+
+    """Class Square inherited from Rectangle"""
+
+    def __init__(self, size):
+        """Instantiation of width and height of square with +ints"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Method that returns area of square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Returns string representation"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
