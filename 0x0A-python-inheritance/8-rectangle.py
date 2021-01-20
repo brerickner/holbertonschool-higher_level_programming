b552cb8cdff9595e8d9aad21d59b4d2b344b3907
brerickner <breannarickner@gmail.com>
2021-01-20 01:30:39 +0000

    output same as main. pepfixed. docstrings good

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module defining BaseGeometry class"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+
+    """Class Rectangle inherited from BaseGeomety"""
+
+    def __init__(self, width, height):
+        """Instantiation of width and height"""
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)
