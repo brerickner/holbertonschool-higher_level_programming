741b5f3d72c012a314d223343bcf0eda468100fb
brerickner <breannarickner@gmail.com>
2021-01-20 23:13:37 +0000

    output good, pepfixed, docstrings good

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,14 +7,13 @@ from os import path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-if path.exists("add_item.json"):
-    pythonList = load_from_json_file('add_item.json')
+addToFile = "add_item.json"
+if path.exists(addToFile):
+    pythonList = load_from_json_file(addToFile)
 else:
     pythonList = []
 
 args = sys.argv[1:]
-print(args)
 pythonList.extend(args)
 
 save_to_json_file(pythonList, 'add_item.json')
