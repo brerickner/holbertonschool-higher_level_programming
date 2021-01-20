9aaa8bf26fe4febb23d1356e5043b137c4ebf2ba
brerickner <breannarickner@gmail.com>
2021-01-20 21:06:50 +0000

    updated docstring

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -7,8 +7,9 @@ import json
 def from_json_string(my_str):
     """Method that gets an obj representation by a JSON string
         Args:
-            my_obj: object to get JSON representation of
+            my_str: str to represent as JSON object
         Returns:
             an obj(Python data stucture) repr by a JSON string
         """
     return json.loads(my_str)
+    
