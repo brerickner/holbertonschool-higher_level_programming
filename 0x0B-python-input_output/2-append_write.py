663aeeed4f62d8779d6f040843a47c635dbb0255
brerickner <breannarickner@gmail.com>
2021-01-20 19:44:37 +0000

    output same as main file, pepfixed, docstrings good

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module defining append_write method"""
+
+
+def append_write(filename="", text=""):
+    """Method that appends a string to end txt file
+        Args:
+            filename: file to append to
+            text: text to append
+        Returns:
+            number of chars added
+        """
+    with open("file_append.txt", 'a', encoding="utf-8") as f:
+        charsAdded = f.write(text)
+        return charsAdded
