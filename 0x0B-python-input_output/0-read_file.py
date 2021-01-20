8dab127bba206bfbc72f3c12cd27492642986ac0
brerickner <breannarickner@gmail.com>
2021-01-20 22:50:18 +0000

    output wasn't as correct as I thought, print without newline

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ def read_file(filename=""):
             filename: file to read from
         """
     with open(filename, 'r', encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
