0dac6b00b2b47d0ce07ea291e40df3f1d7efb64d
brerickner <breannarickner@gmail.com>
2021-01-20 19:17:17 +0000

    output same as main example, docstrings good, pepfixed

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module defining write_file method"""
+
+
+def write_file(filename="", text=""):
+    """Method that writes a string to txt file
+        Args:
+            filename: file to read from
+            text: text to write
+        Returns:
+            number of chars written
+        """
+    with open("my_first_file.txt", 'w', encoding="utf-8") as f:
+        numOfChars = f.write(text)
+        return numOfChars
