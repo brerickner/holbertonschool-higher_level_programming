7503f6ed43aeddeb2a1cec712a80b914e59848cd
brerickner <breannarickner@gmail.com>
2021-01-20 23:14:34 +0000

    seeing if it makes dif if filename is declared as variable or not

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,9 +7,8 @@ from os import path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-addToFile = "add_item.json"
-if path.exists(addToFile):
-    pythonList = load_from_json_file(addToFile)
+if path.exists("add_item.json"):
+    pythonList = load_from_json_file('add_item.json')
 else:
     pythonList = []
 
