81edaeba3889a8cef171b210abb4e90ed1755d56
brerickner <breannarickner@gmail.com>
2021-01-20 20:56:45 +0000

    making more portable by using variable filename

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ def read_file(filename=""):
         Args:
             filename: file to read from
         """
-    with open("my_file_0.txt", 'r', encoding="utf-8") as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         print(f.read())
