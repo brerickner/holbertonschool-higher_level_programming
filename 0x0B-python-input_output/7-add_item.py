6772df7a6793c43fcc38b83ede38e93acbe8386d
brerickner <breannarickner@gmail.com>
2021-01-20 23:08:22 +0000

    output good, pepfixed, docstrings good

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,17 +2,21 @@
 """Module that adds and saves Python list to file"""
 
 import sys
-import os
+import os.path
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-args = sys.argv[1:]
+
 if path.exists("add_item.json")
     pythonList = load_from_json_file('add_item.json')
+    print("in if")
 else:
+    print("in else")
     pythonList = []
 
+args = sys.argv[1:]
+print(args)
 pythonList.extend(args)
 
 save_to_json_file(pythonList, 'add_item.json')
