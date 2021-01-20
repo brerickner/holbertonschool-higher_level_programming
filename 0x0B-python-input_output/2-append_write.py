ccfe2065ecfea64c48941225838f0e14582650f8
brerickner <breannarickner@gmail.com>
2021-01-20 20:56:22 +0000

    making more portable by using variable filename

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -10,6 +10,6 @@ def append_write(filename="", text=""):
         Returns:
             number of chars added
         """
-    with open("file_append.txt", 'a', encoding="utf-8") as f:
+    with open(filename, 'a', encoding="utf-8") as f:
         charsAdded = f.write(text)
         return charsAdded
