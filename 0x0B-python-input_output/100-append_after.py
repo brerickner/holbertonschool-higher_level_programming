982a1d27d671aabcafc691c354404b17c558cccf
brerickner <breannarickner@gmail.com>
2021-01-21 05:06:39 +0000

    changed from readlines() to iterating through adding lines to list

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -6,9 +6,11 @@ def append_after(filename="", search_string="", new_string=""):
     """Method that inserts a line of txt after spec string"""
     with open(filename, encoding="utf-8") as f:
         text = []
-        lineRead = f.readlines()
-        if search_string in lineRead:
-            text.append(new_string)
+        for lineRead in f:
+            text.append(lineRead)
+            if search_string in lineRead:
+                text.append(new_string)
 
+    with open(filename, 'w', encoding='utf-8') as f:
         for lineRead in text:
             f.write(lineRead)
