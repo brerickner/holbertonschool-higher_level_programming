6cb36d63520053ac69d6f78df140f9dc5eebb007
brerickner <breannarickner@gmail.com>
2021-01-24 02:41:15 +0000

    saving progress

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -26,7 +26,34 @@ class TestRectangleClass(unittest.TestCase):
     ValueError: height must be > 0 
     
     r1 = Rectangle(None, None)
+    TypeError: width must be an integer
+
+    r1 = Rectangle(2, 2)
+    
+    r4 = Rectangle(None, 10)
+    print(r4.area())
+    width must be an integer
+
+    r4 = Rectangle(1.1, 10)
+    print(r4.area())
+    width must be an integer
+
+    r4 = Rectangle(1, 10.00000000000000000)
+    print(r4.area())
     TypeError: height must be an integer
+
+    r4 = Rectangle(1, -1.0)
+    print(r4.area())
+    ValueError: height must be > 0 
+
+    r4 = Rectangle('x', 3)
+    print(r4.area())
+    TypeError: width must be an integer
+
+TESTS FOR X, Y
+    r3 = Rectangle(3, 3, None, None)
+    print(r2)
+    TypeError: x must be an integer
     
     
     
