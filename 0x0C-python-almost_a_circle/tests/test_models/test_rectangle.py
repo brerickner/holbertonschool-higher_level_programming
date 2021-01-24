a7657f8e8af2477c3582a2aff7dc797f0e4cd0c7
brerickner <breannarickner@gmail.com>
2021-01-24 01:39:50 +0000

    saving prog

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -2,18 +2,36 @@
 """Module with unittest methods for base.py"""
 
 import unittest
-#from models.base import Base
 from models.rectangle import Rectangle
 
 class TestRectangleClass(unittest.TestCase):
-    """Class that contains unittest methods for Rectangle module"""\
+    """Class that contains unittest methods for Rectangle module"""
     
-    
-
-
     def test_rectangle_id(self):
+        """Method to test the id of Rectangle"""
         r1 = Rectangle(10, 2)
         self.assertEqual(r1.id, 1)
+
+"""Method to raise exceptions for width/height"""
+    """r1 = Rectangle(-2, 2)
+    ValueError: width must be > 0 
+
+    r1 = Rectangle(0, 2)
+    ValueError: width must be > 0
+
+    r1 = Rectangle(0, 0)
+    ValueError: width must be > 0 
+    
+    r1 = Rectangle(7, -1)
+    ValueError: height must be > 0 
+    
+    r1 = Rectangle(None, None)
+    TypeError: height must be an integer
+    
+    
+    
+    """
+
 
 
 
