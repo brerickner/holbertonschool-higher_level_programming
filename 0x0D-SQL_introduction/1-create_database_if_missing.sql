b7c8d54ffaa507faff326c119aad236d6399be24
brerickner <breannarickner@gmail.com>
2021-03-01 21:51:02 +0000

    forgot to semicolon

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,2 @@
 -- script that creates the database hbtn_0c_0
-IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'hbtn_0c_0')
-    CREATE DATABASE hbtn_0c_0;
+CREATE DATABASE [IF NOT EXISTS] hbtn_0c_0;
