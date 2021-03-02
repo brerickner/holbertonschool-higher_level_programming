ad7e3a5b198b55ddf3a8e65bb88dc429ccef7d1a
brerickner <breannarickner@gmail.com>
2021-03-02 21:27:52 +0000

    adding task 8 out put looks good.

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,8 +2,8 @@
 -- be found in the database hbtn_0d_usa.
 SELECT id, name
 FROM hbtn_0d_usa.cities
-WHERE state_id =
-(SELECT id
+WHERE state_id IN (
+SELECT id
 FROM hbtn_0d_usa.states
-WHERE name = 'California')
+WHERE states.name = 'California')
 ORDER BY cities.id ASC;
