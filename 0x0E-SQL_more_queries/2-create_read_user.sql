efa07f1d1f42129488fbec7d2c1142679b786076
brerickner <breannarickner@gmail.com>
2021-03-02 18:36:39 +0000

    taking out single quotes

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' 
+IDENTIFIED BY 'user_0d_2_pwd';
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
