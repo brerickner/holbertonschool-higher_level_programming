5c1c91af55932af9d5f9866feda9114ad35754ae
brerickner <breannarickner@gmail.com>
2021-03-02 21:47:09 +0000

    taking out hbtn per ben's suggestion

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,9 @@
 -- a script that lists all the cities of California that can
 -- be found in the database hbtn_0d_usa.
 SELECT cities.id, cities.name
-FROM hbtn_0d_usa.cities
+FROM cities
 WHERE cities.state_id IN (
 SELECT states.id
-FROM hbtn_0d_usa.states
+FROM states
 WHERE states.name = 'California')
 ORDER BY cities.id ASC;
