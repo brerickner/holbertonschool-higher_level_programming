5f6f8094ffa49cf47f286c0abdeea56800d18264
brerickner <breannarickner@gmail.com>
2021-03-22 19:39:29 +0000

    adding task 1, unable to test semistandard

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+let sentence;
+let first = 'C is fun\n';
+let second = 'Python is cool\n';
+let third = 'JavaScript is amazing';
+
+sentence = first + second + third;
+
+console.log(sentence);
