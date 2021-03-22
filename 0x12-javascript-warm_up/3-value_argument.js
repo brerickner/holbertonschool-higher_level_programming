4598d5e6eda38d3240fb9fd3d101ace130dd16e0
brerickner <breannarickner@gmail.com>
2021-03-22 23:06:08 +0000

    adding task 3. passing semistandard

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,3 +2,11 @@
 
 const theArg = process.argv[2];
 theArg === undefined ? console.log('No argument') : console.log(theArg);
+
+/* ____SAME AS _____
+if (theArg === undefined) {
+  console.log('No argument');
+} else {
+  console.log(theArg);
+}
+*/
