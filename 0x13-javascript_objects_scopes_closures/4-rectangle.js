95ef282851b0d39c4d776bcd4254470659cb9e4d
brerickner <breannarickner@gmail.com>
2021-03-23 19:29:18 +0000

    adding task 4. semistandard good and correct output

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    const x = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(`${x}`.repeat(this.width));
+    }
+  }
+  rotate() {
+    let buff = this.width;
+    this.width = this.height;
+    this.height = buff;
+  }
+  double() {
+    this.width *=  2;
+    this.height *=  2;
+    }
+  }
+module.exports = Rectangle;
