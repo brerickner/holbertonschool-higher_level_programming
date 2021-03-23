77b091dc98212053872a07162dd05b8d99c414f8
brerickner <breannarickner@gmail.com>
2021-03-23 04:08:04 +0000

    ohhhh it want's the second largest! correcting code

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,14 +3,15 @@
 const numArgs = process.argv;
 const argLength = numArgs.length;
 
-argLength < 4 ? console.log(0) : console.log(numArgs.sort()[argLength - 1]);
+argLength < 4 ? console.log(0) : console.log(numArgs.sort()[argLength - 2]);
 
 /*  ___SAME AS____
 let numArgs = process.argv;
 let argLength = numArgs.length;
+
 if (argLength < 4) {
-console.log(0);
+    console.log(0);
 } else {
-console.log(numArgs.sort()[argLength - 1]);
+    console.log(numArgs.sort()[argLength - 2]);
 }
 */
