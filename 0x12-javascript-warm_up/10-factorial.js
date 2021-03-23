9f3902bf165d8600aa3a0a6d3da4b9a832ea7d29
brerickner <breannarickner@gmail.com>
2021-03-23 03:49:40 +0000

    casting integer when assigning variable to first argument

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,20 @@
 #!/usr/bin/node
 
-const theNum = process.argv[2];
+const theNum = parseInt(process.argv[2]);
 
 function factorial (num) {
-  if (num === 1 || isNaN(num)) {
-    return (1);
-  } else {
-    return parseInt((factorial(num - 1) * num));
-  }
+  return num === 1 || isNaN(num) ? 1 : factorial(num - 1) * num;
 }
 console.log(factorial(theNum));
+
+/*
+let theNum = parseInt(process.argv[2]);
+function factorial(num) {
+if (num < 2 || isNaN(num)) {
+return (1);
+} else {
+return ((factorial(num - 1) * num));
+}
+}
+console.log(factorial(theNum));
+*/
