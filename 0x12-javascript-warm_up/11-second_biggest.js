78d052c8f612581fec151442ef2b19d616be07c6
brerickner <breannarickner@gmail.com>
2021-03-23 04:05:49 +0000

    adding task 11. semifixed and output good

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const numArgs = process.argv;
+const argLength = numArgs.length;
+
+argLength < 4 ? console.log(0) : console.log(numArgs.sort()[argLength - 1]);
+
+/*  ___SAME AS____
+let numArgs = process.argv;
+let argLength = numArgs.length;
+if (argLength < 4) {
+console.log(0);
+} else {
+console.log(numArgs.sort()[argLength - 1]);
+}
+*/
