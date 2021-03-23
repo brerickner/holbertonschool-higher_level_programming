e60c91b3c492bdd4d4873c0bfc32845da93aa1a3
brerickner <breannarickner@gmail.com>
2021-03-23 21:50:37 +0000

    adding edited task 7. accounting for test when there are no reoccurrences in the list

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-  let result = '';
+  let result = 0;
   for (const i in list) {
     if (list[i] === searchElement) {
       result++;
