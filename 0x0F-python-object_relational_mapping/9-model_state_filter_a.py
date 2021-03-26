d0bedd288d6033a0eefbdb544d6d1a9a38e2cce9
brerickner <breannarickner@gmail.com>
2021-03-26 21:56:52 +0000

    adding task 9. output good. pepfixed

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-'''Module with script that lists all State objects
- from the database hbtn_0e_6_usa)'''
+'''script that lists all State objects that contain the
+letter a from the database hbtn_0e_6_usa'''
 
 from sys import argv
 from model_state import State, Base
