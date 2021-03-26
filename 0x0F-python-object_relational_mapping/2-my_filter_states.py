2d48678dac52d1ea242aefb0b8b5422f2ccef654
brerickner <breannarickner@gmail.com>
2021-03-26 00:56:02 +0000

    adding task 2. output okay. pepfixed

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''Module with script that displays state names matching the argument'''
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == sys.argv[4]:
+            print("{}".format(row))
+    # Close all cursors
+    cur.close()
+    # Close all databases
+    db.close()
