4b90c93e3d5e4e222b52f237072b1f3ee05e978c
brerickner <breannarickner@gmail.com>
2021-03-26 21:52:33 +0000

    adding task 9. output good. pepfixed

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''Module with script that lists all State objects
+ from the database hbtn_0e_6_usa)'''
+
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+vroom = 'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3])
+
+if __name__ == '__main__':
+    engine = create_engine(vroom)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for instance in session.query(State).order_by(
+            State.id).filter(
+            State.name.contains('a')):
+        try:
+            print("{}: {}".format(instance.id, instance.name))
+        except BaseException:
+            print("Nothing")
+    session.close()
