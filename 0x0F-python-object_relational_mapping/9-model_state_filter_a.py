030603627a579c073e7cf698d9a2c819b71dc2e0
brerickner <breannarickner@gmail.com>
2021-03-28 03:12:31 +0000

    9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -4,12 +4,12 @@ letter a from the database hbtn_0e_6_usa'''
 
 from sys import argv
 from model_state import State, Base
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
-vroom = 'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3])
-
 if __name__ == '__main__':
+    vroom = 'mysql+mysqldb://{}:{}@localhost/{}'\
+        .format(argv[1], argv[2], argv[3])
     engine = create_engine(vroom)
     Base.metadata.create_all(engine)
 
