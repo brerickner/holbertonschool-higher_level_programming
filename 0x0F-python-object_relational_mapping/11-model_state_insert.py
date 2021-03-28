1dd12914f66d5ec9758cb61709e3b8ef11c6be08
brerickner <breannarickner@gmail.com>
2021-03-28 03:36:04 +0000

    11-model_state_insert.py

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -3,13 +3,15 @@
 to the database hbtn_0e_6_usa'''
 
 from sys import argv
-from model_state import State, Base
-from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
-vroom = 'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3])
+
 
 if __name__ == '__main__':
+    vroom = 'mysql+mysqldb://{}:{}@localhost/{}'\
+        .format(argv[1], argv[2], argv[3])
     engine = create_engine(vroom)
     Base.metadata.create_all(engine)
 
