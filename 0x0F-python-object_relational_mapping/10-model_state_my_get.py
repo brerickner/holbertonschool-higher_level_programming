e08e6e89a76b64faf97e2964b4b70e80c1e78f86
brerickner <breannarickner@gmail.com>
2021-03-28 03:14:50 +0000

    10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -4,7 +4,7 @@ as argument from the database hbtn_0e_6_usa'''
 
 from sys import argv
 from model_state import State, Base
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 
