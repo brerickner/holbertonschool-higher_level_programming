9827ca91f9a9f66581f72b85ba29d68247bc1fe0
brerickner <breannarickner@gmail.com>
2021-04-08 20:12:24 +0000

    shellchecked forgot to take in argument

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends request and displays size of body
-curl -Is localhost:5000 | grep "Content-Length" | cut -d " " -f2
+curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
