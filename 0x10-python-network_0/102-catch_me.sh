40eb842beba1f83b05b64e0acf08e8251e14b1cc
brerickner <breannarickner@gmail.com>
2021-04-08 23:13:30 +0000

    and the s

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that makes a request to 0.0.0.0:5000/catch_me prints You got me!
-curl -L 0.0.0.0:5000/catch_me -X PUT -d "catch_me=You got me\!&user_id=98" -H "Origin: HolbertonSchool"
+curl -sL 0.0.0.0:5000/catch_me -X PUT -d "catch_me=You got me\!&user_id=98" -H "Origin: HolbertonSchool"
