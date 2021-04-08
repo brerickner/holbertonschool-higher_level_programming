739a151ef883e0c758fc0ddaaa4e846a775a3da0
brerickner <breannarickner@gmail.com>
2021-04-08 21:49:50 +0000

    oh my gosh. executable. changed back to original

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a request passed as argument then displays status code
-curl -s -o /dev/null -w "%{http_code}" "$1"
+curl -s -o /dev/null -I -w "%{http_code}" "$1"
