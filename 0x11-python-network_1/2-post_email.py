883d2ddc418fbcc82bb75fd4a47f5d00a8b77a7a
brerickner <breannarickner@gmail.com>
2021-04-09 19:31:30 +0000

    oops pepfixed this time

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''script that takes in a URL and an email, sends a POST request to the 
+'''script that takes in a URL and an email, sends a POST request to the
 passed URL with the email as a parameter, and displays the body of the
 response (decoded in utf-8)'''
 
@@ -18,4 +18,4 @@ if __name__ == "__main__":
 
     with urllib.request.urlopen(url, data) as response:
         meow = response.read()
-        print ("{}".format(meow.decode(encoding='UTF-8')))
+        print("{}".format(meow.decode(encoding='UTF-8')))
